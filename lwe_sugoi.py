6046b9fd03e2183f16ef7695abc63c54c103371f
aidatorajiro <kawarusosu@zoho.com>
2019-12-10 01:56:52 +0900

    update lwe

--- lwe_sugoi.py
+++ lwe_sugoi.py
@@ -1,3 +1,5 @@
+# from https://en.wikipedia.org/wiki/Learning_with_errors#Public-key_cryptosystem
+
 import math
 import random
 from functools import reduce
@@ -25,45 +27,60 @@ def add_vectors(matrix):
     return reduce(add, matrix, [0] * len(matrix[0]))
 
 ### consts
-n = 128
-p = random.choice(list(filter(lambda x: x >= n*n, prime(2*n*n))))
-#eps = (3.45678910111213141516)
-#m = (1 + eps)*(n + 1)*math.log(p)
-m = 5*n
-alp = lambda n: 1/(math.sqrt(n)*math.log(n))
-x = lambda: random.normalvariate(0, alp(n)/math.sqrt(2*math.pi)) % 1.0
+def genconsts(n = 128):
+    p = random.choice(list(filter(lambda x: x >= n*n, prime(2*n*n))))
+    #eps = (3.45678910111213141516)
+    #m = (1 + eps)*(n + 1)*math.log(p)
+    m = 5*n
+    alp = lambda n: 1/(math.sqrt(n)*math.log(n))
+    x = lambda: random.normalvariate(0, alp(n)/math.sqrt(2*math.pi)) % 1.0
 
-### priv key
-s = [random.randint(0, p - 1) for i in range(n)]
+    ### priv key
+    s = [random.randint(0, p - 1) for i in range(n)]
 
-### public key
-a = [[random.randint(0, p - 1) for i in range(n)] for i in range(m)]
-e = [x() for i in range(m)]
-b = [dot(a[i], s) + e[i] for i in range(m)]
+    ### public key
+    a = [[random.randint(0, p - 1) for i in range(n)] for i in range(m)]
+    e = [x() for i in range(m)]
+    b = [dot(a[i], s) + e[i] for i in range(m)]
+
+    return (m, a, b, p, s)
 
 ### encryption
-bit = 0
-print(bit)
+def encryption(bit, m, a, b, p):
+    S = list(filter(lambda x: random.random() < 0.5, range(m)))
 
-S = list(filter(lambda x: random.random() < 0.5, range(m)))
+    if bit == 0:
+        enc_a = add_vectors([a[i] for i in S])
+        enc_b = sum([b[i] for i in S])
+    else:
+        enc_a = add_vectors([a[i] for i in S])
+        enc_b = (p // 2) + sum([b[i] for i in S])
 
-if bit == 0:
-    enc_a = add_vectors([a[i] for i in S])
-    enc_b = sum([b[i] for i in S])
-else:
-    enc_a = add_vectors([a[i] for i in S])
-    enc_b = (p // 2) + sum([b[i] for i in S])
+    enc = (enc_a, enc_b)
 
-enc = (enc_a, enc_b)
-print(enc)
+    return enc
 
 ### decryption
 
-if enc[1] - dot(enc[0], s) < p//2:
-    dec = 0
-else:
-    dec = 1
+def decryption(enc, s, p):
+    if enc[1] - dot(enc[0], s) < p//2:
+        dec = 0
+    else:
+        dec = 1
+    
+    return dec
 
-print(dec)
+### some test ...
 
-### some test...
+(m, a, b, p, s) = genconsts()
+samples = 100
+for i in range(samples):
+    bit = random.randint(0, 1)
+    enc = encryption(bit, m, a, b, p)
+    dec = decryption(enc, s, p)
+
+    if bit == dec:
+        print("ok")
+    else:
+        print("error")
+    
